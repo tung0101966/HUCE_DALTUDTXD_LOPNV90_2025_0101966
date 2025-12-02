98697126c685ad86a100ffd842b2f62ec191ee12
tung0101966 <0101966@st.huce.edu.vn>
2025-12-02 13:39:54 +0700

    tung0101966/Feature/bangTra_IV_1

---
+++ HUCE_DALTUDTXD_LOPNV90_2025_0101966/Class/BangTra_IV-1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhToanCoc.Class
+{
+    public class clsBangTra_IV_1
+    {
+        public int ChieuSau { get; set; }        // Chiều sâu
+        public double[] GiaTri { get; set; }    // Mảng giá trị T_i theo I_L
+
+        // Constructor
+        public clsBangTra_IV_1(int chieuSau, double[] giaTri)
+        {
+            ChieuSau = chieuSau;
+            GiaTri = giaTri;
+        }
+    }
+}
